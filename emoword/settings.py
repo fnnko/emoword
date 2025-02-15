f14e8f67ad1979e06244bca69db1befade306dbd
fuka <fuuka171016@gmail.com>
2025-02-15 17:50:34 +0900

    3rd commit

--- emoword/settings.py
+++ emoword/settings.py
@@ -118,7 +118,16 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# ...
+try:
+    from .local_settings import *
+except ImportError:
+    # local_settings.py が存在しなくてもエラーにならないようにする
+    pass
